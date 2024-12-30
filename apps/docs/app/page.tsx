dd476159daaf70c865bff5b0e9b59bd9793e14b0
huccct <ucccth@gmail.com>
2024-12-30 22:25:07 +0000

    chore: fix eslint

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,5 +1,5 @@
-import { Header } from './components/Header'
 import { Canvas } from './components/Canvas'
+import { Header } from './components/Header'
 
 export default function Page() {
   return (
