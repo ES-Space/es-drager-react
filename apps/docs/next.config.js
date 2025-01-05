e3461a249c43089bdac316f5b31b0e2bed560f90
huccct <ucccth@gmail.com>
2025-01-05 15:26:17 +0000

    chore: fix style

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -2,7 +2,7 @@ import process from 'node:process'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/es-drager-react/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/es-drager-react' : '',
   images: {
     unoptimized: true,
   },
