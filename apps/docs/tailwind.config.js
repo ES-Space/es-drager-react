1ba70866a6b664b8953e375f3348cf5e358b7651
huccct <ucccth@gmail.com>
2025-01-05 15:35:31 +0000

    chore: fix style

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,4 +1,3 @@
-import type { Config } from 'tailwindcss'
 import typography from '@tailwindcss/typography'
 
 export default {
@@ -9,4 +8,4 @@ export default {
     extend: {},
   },
   plugins: [typography],
-} satisfies Config
+}
