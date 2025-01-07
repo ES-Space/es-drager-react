3747ffad541ca5131a96162d46af89bfbe532c0d
huccct <ucccth@gmail.com>
2025-01-07 23:23:41 +0000

    chore: fix ci

--- apps/docs/env.ts
+++ apps/docs/env.ts
@@ -1,3 +1,2 @@
-import process from 'node:process'
-
+// eslint-disable-next-line node/prefer-global/process
 export const BASE_PATH = process.env.NODE_ENV === 'production' ? '/es-drager-react' : ''
