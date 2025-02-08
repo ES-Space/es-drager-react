2f4d9d828ffcac20f8132171b3ac234d160a5bf7
huccct <ucccth@gmail.com>
2025-02-08 23:02:21 +0000

    chore: fix build

--- packages/react-drager/tsup.config.ts
+++ packages/react-drager/tsup.config.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
@@ -6,14 +7,22 @@ export default defineConfig({
   dts: true,
   splitting: false,
   sourcemap: true,
-  clean: true,
+  clean: false,
   treeshake: true,
   minify: true,
   external: ['react'],
   esbuildOptions(options) {
     options.loader = {
       ...options.loader,
-      '.svg': 'dataurl', // 处理 SVG 文件
+      '.svg': 'dataurl', // handle svg
     }
+  },
+  onSuccess: async () => {
+    const files = ['dist/index.cjs.map', 'dist/index.js.map']
+    files.forEach((file) => {
+      if (fs.existsSync(file)) {
+        fs.unlinkSync(file)
+      }
+    })
   },
 })
